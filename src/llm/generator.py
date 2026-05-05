54687c986fc7b8d9fb1ee0584fbdb26114aecb2b
Hemavathi S <hemaselvam0304.july@gmail.com>
2026-05-05 19:35:32 +0530

    Update generator.py

--- src/llm/generator.py
+++ src/llm/generator.py
@@ -1,23 +1,29 @@
-import streamlit as st
-from groq import Groq 
+# import streamlit as st
+# from groq import Groq 
 #Groq model
 import streamlit as st
 from langchain_groq import ChatGroq
 
-client = Groq(
+client = ChatGroq(
     model="llama-3.1-8b-instant",
     api_key=st.secrets["GROQ_API_KEY"]
 )
 def generate_response(prompt):
-    response = client.chat.completions.create(
-        model = "llama-3.1-8b-instant",
-        messages=[
-            {"role": "user", "content": prompt}
-        ],
-        temperature=0.3
-    )
+    return llm.invoke(prompt).content
 
-    return response.choices[0].message.content
+# def generate_response(prompt):
+#     response = client.chat.completions.create(
+#         model = "llama-3.1-8b-instant",
+#         messages=[
+#             {"role": "user", "content": prompt}
+#         ],
+#         temperature=0.3
+#     )
+
+#     return response.choices[0].message.content
+
+
+
 
 
 
